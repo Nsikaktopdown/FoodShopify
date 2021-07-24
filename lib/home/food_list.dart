a41d51d272b06eb657c695b44a121e90b41bc9cc
NsikakApp <nsikakthompson73@gmail.com>
2021-07-24 22:04:31 +0100

    built foodlist UI

--- lib/home/food_list.dart
+++ lib/home/food_list.dart
@@ -13,8 +13,10 @@ class FoodListState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
+          color: Colors.white,
           child: Column(
             children: [
               SizedBox(width: 20),
@@ -37,7 +39,7 @@ class FoodListState extends State {
                     padding: EdgeInsets.only(
                         top: 10, left: 15, right: 15, bottom: 10),
                     decoration: BoxDecoration(
-                        color: Colors.white,
+                        color: Pallet.white,
                         borderRadius: BorderRadius.all(Radius.circular(30.0))),
                     child: Text(
                       "Pickup",
@@ -46,7 +48,7 @@ class FoodListState extends State {
                   )
                 ],
               ),
-              _foodItem()
+              Expanded(child: foodList())
             ],
           ),
         ),
@@ -54,8 +56,20 @@ class FoodListState extends State {
     );
   }
 
-  Widget _foodItem() {
+  Widget foodList() {
     return Container(
+      child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return _foodItem();
+          }),
+    );
+  }
+
+  Widget _foodItem() {
+    return Card(
+        child: Container(
+      padding: EdgeInsets.all(10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -66,27 +80,70 @@ class FoodListState extends State {
             width: Utils.getDeviceWidth(context),
             fit: BoxFit.cover,
           )),
+          SizedBox(
+            height: 10,
+          ),
           Container(
             child: Row(
               children: [
                 Text("Indian Salad Receipe",
-                    style: TextStyle(color: Colors.black)),
+                    style: TextStyle(
+                        color: Pallet.mainTextDarkColor, fontSize: 16)),
+                Spacer(),
                 Container(
                   padding:
                       EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 10),
                   decoration: BoxDecoration(
-                      color: Pallet.textLight,
+                      color: Pallet.newItemBackgroundColor,
                       borderRadius: BorderRadius.all(Radius.circular(30.0))),
                   child: Text(
                     "4.4",
-                    style: TextStyle(color: Colors.black, fontSize: 6),
+                    style: TextStyle(color: Colors.black, fontSize: 8),
                   ),
-                )
+                ),
               ],
             ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text("\$\$ * Sandwiches * Salad",
+              style: TextStyle(color: Pallet.textLight)),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding:
+                    EdgeInsets.only(top: 6, left: 10, right: 10, bottom: 6),
+                decoration: BoxDecoration(
+                    color: Pallet.newItemBackgroundColor,
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                child: Text(
+                  "20-30 Min",
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Container(
+                padding:
+                    EdgeInsets.only(top: 6, left: 10, right: 10, bottom: 6),
+                decoration: BoxDecoration(
+                    color: Pallet.newItemBackgroundColor,
+                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                child: Text(
+                  "\$20.00 Fee",
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+              ),
+            ],
           )
         ],
       ),
-    );
+    ));
   }
 }
